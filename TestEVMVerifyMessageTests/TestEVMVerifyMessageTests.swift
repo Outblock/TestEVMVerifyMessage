924fd7fa2bb2bbf99a65c7f2cc48436994a81004
lmcmz <lmcmze@gmail.com>
2024-10-09 15:46:23 +1100

    Initial Commit

---
+++ TestEVMVerifyMessageTests/TestEVMVerifyMessageTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestEVMVerifyMessageTests.swift
+//  TestEVMVerifyMessageTests
+//
+//  Created by Hao Fu on 9/10/2024.
+//
+
+import Testing
+@testable import TestEVMVerifyMessage
+
+struct TestEVMVerifyMessageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
